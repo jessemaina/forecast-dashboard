f499be2afefed28a272e777b4a370bd84f1fa6ee
jessemaina <32290398+jessemaina@users.noreply.github.com>
2025-05-29 00:37:39 +0800

    Update forecast_dashboard.py

--- forecast_dashboard.py
+++ forecast_dashboard.py
@@ -17,7 +17,7 @@ from uber_demand_forecast import generate_forecast
 st.set_page_config(page_title="Weather Dashboard", layout="wide")
 
 # === Header ===
-st.title("🌦️ Your Daily Forecast Assistant")
+st.title("🌦️")
 st.markdown(f"**Date:** {datetime.now().strftime('%A, %B %d, %Y')}")
 st.markdown("---")
 
